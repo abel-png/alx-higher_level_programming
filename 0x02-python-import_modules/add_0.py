ac815fe285f5e4aaa92ae7f11a4c34974d5ad5f6
abel-png <abelteshome01@gmail.com>
2023-03-10 19:24:31 +0000

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+       if __name__ == "__main__":
+	def name ():
+
+    Args:
+        a: first integer
+	b: second integer
+    Returns:
+	The return value. a + b
+    """
+    return (a + b)
+
