8fd66d5c125bfe8a96cfe41d1065645c5fbcab52
abel-png <abelteshome01@gmail.com>
2023-03-10 20:01:14 +0000

    import calculator module

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,39 @@
+#!/bin/bash/python3
+def add(a, b):
+    """My addition function
+    Args:
+        a: first integer
+	b: second integer
+
+    Returns:
+	The return value. a + b
+    """
+    return (a+b)
+def sub(a, b):
+    """My subtraction function
+        Args:
+	   a: first integer
+	   b: second integer
+
+	Returns:
+	   The return value. a - b
+    """
+    return (a - b)
+def mul(a, b):
+    """My multiplication function
+    Args:
+        a: first integer
+        b: second integer
+     returns:
+        The return value. a * b
+     """
+    return (a * b)
+def div(a, b):
+    """My division function
+    Args:
+	a: first integer
+	b: second integer
+    returns:
+	the return value. a/b
+    """
+    return int(a/b)
